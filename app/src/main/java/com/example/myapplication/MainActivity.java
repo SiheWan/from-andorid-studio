9df4232e32007e9de9d114b750f68eec53f2d88f
Sihe Wan <sihe@ualberta.ca>
2022-03-07 21:26:23 -0700

    test commit from SiheWan

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //edit from local SiheWan local
     }
 }
